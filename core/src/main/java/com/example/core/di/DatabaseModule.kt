7ccb14a4a854ed22f7bb20386118ac43ed16afa0
artasrikartikasianturi <breslisianturi@yahoo.com>
2021-11-13 17:11:20 +0700

    add obfuscation with proguard, implement encryption in database and implement certificate pinning in network connection

--- core/src/main/java/com/example/core/di/DatabaseModule.kt
+++ core/src/main/java/com/example/core/di/DatabaseModule.kt
@@ -7,17 +7,22 @@ import dagger.Provides
 import com.example.core.data.source.local.room.FavoriteMovieTvDao
 import com.example.core.data.source.local.room.MovieTvDao
 import com.example.core.data.source.local.room.MovieTvDatabase
+import net.sqlcipher.database.SQLiteDatabase
+import net.sqlcipher.database.SupportFactory
 import javax.inject.Singleton
 
 @Module
 class DatabaseModule {
+
+    private val passphrase: ByteArray = SQLiteDatabase.getBytes("dicoding".toCharArray())
+    val factory = SupportFactory(passphrase)
 
     @Singleton
     @Provides
     fun provideDatabase(context: Context): MovieTvDatabase = Room.databaseBuilder(
         context,
         MovieTvDatabase::class.java, "MovieTvLocal.db"
-    ).fallbackToDestructiveMigration().build()
+    ).fallbackToDestructiveMigration().openHelperFactory(factory).build()
 
     @Provides
     fun provideMovieTvDao(database: MovieTvDatabase): MovieTvDao = database.movieTvDao()
--- core/src/main/java/com/example/core/di/NetworkModule.kt
+++ core/src/main/java/com/example/core/di/NetworkModule.kt
@@ -4,6 +4,7 @@ import com.example.core.BuildConfig
 import com.example.core.data.source.remote.network.ApiService
 import dagger.Module
 import dagger.Provides
+import okhttp3.CertificatePinner
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
@@ -12,6 +13,16 @@ import java.util.concurrent.TimeUnit
 
 @Module
 class NetworkModule {
+    /**
+     * create certificate in https://www.ssllabs.com/analyze.html?d=api.themoviedb.org&s=13.227.76.33&latest
+     */
+    private val hostname = "api.themoviedb.org"
+    private val certificatePinner = CertificatePinner.Builder()
+        .add(hostname, "sha256/oD/WAoRPvbez1Y2dfYfuo4yujAcYHXdv1Ivb2v2MOKk=")
+        .add(hostname, "sha256/oD/JSMzqOOrtyOT1kmau6zKhgT676hGgczD5VMdRMyJZFA=")
+        .add(hostname, "sha256/++MBgDH5WGvL9Bcn5Be30cRcL0f5O+NyoXuWtQdX1aI=")
+        .add(hostname, "sha256/KwccWaCgrnaw6tsrrSO61FgLacNgG2MMLq8GE6+oP5I=")
+        .build()
 
     @Provides
     fun provideOkHttpClient(): OkHttpClient {
@@ -20,6 +31,7 @@ class NetworkModule {
             .connectTimeout(120, TimeUnit.SECONDS)
             .readTimeout(120, TimeUnit.SECONDS)
             .writeTimeout(120, TimeUnit.SECONDS)
+            .certificatePinner(certificatePinner = certificatePinner)
             .build()
     }
 
